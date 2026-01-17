2fa5067a48aca6de35a65f7ba63682ec22ee34e2
Tiwari Shubham <Shubham.injoria@gmail.com>
2026-01-17 18:33:41 +0530

    update

---
+++ ES6+ Features/Beginner Level/11_solution.js
@@ -0,0 +1,8 @@
+// Implement a JavaScript program to use for...of loop for iterating arrays. (Concept: Loops)
+
+
+const fruits = ["Apple", "Banana", "Mango", "Orange", "Pineapple"];
+
+for (const element of fruits) {
+    console.log(element)
+}
---
+++ ES6+ Features/Beginner Level/12_solution.js
@@ -0,0 +1,5 @@
+// Create a JavaScript function to use Object.assign() for object cloning. (Concept: Object Methods)
+
+const user = {
+    
+}
