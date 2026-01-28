c784f3ad9820a4d962bee51af18e7fe748029b05
Tiwari Shubham <Shubham.injoria@gmail.com>
2026-01-28 16:48:37 +0530

    add

--- ES6+ Features/Beginner Level/12_solution.js
+++ ES6+ Features/Beginner Level/12_solution.js
@@ -1,5 +1,11 @@
 // Create a JavaScript function to use Object.assign() for object cloning. (Concept: Object Methods)
 
 const user = {
-    
+    firstname : "Shubham",
+    lastname : "Tiwari",
 }
+
+
+const clonedUser = Object.assign({}, user);
+
+console.log(clonedUser);
