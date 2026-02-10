b44d7028df6206223d5563ee4b73c6323b63d1c0
Tiwari Shubham <Shubham.injoria@gmail.com>
2026-02-10 23:18:13 +0530

    add

---
+++ ES6+ Features/Intermediate Level/226_solutions.js
@@ -0,0 +1,23 @@
+// Write a JavaScript function to implement a class with getters and setters. (Concept: Classes)
+
+
+class VoterUser {
+    constructor(username, age){
+        this.username = username;
+        this.age = age;
+    }
+
+    get age() {
+        this.age
+    }
+
+    set age(value){
+        if(value < 18){
+            throw new Error(`Enter correct user age `)
+        }
+        this.age = value;
+    }
+}
+
+const userA = new VoterUser("ProgrammerBanda", 16)
+console.log(userA)
